5bdedf709370188c0363aae2cc0680a0b18eb74d
Ali Bouzar <alibouzar@foi.hr>
2022-03-12 23:10:16 +0100

    Adding custom exceptions

--- src/main/java/net/miaoubich/controller/advice/MyControllerAdvice.java
+++ src/main/java/net/miaoubich/controller/advice/MyControllerAdvice.java
@@ -2,28 +2,40 @@ package net.miaoubich.controller.advice;
 
 import java.util.NoSuchElementException;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import net.miaoubich.exception.BusinessException;
 
 @ControllerAdvice
-public class MyControllerAdvice {
+public class MyControllerAdvice extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(BusinessException.class)
-	public ResponseEntity<String> handleEmptyFields(BusinessException businessException){
+	public ResponseEntity<String> handleEmptyFields(BusinessException businessException) {
 		return new ResponseEntity<String>("Fields are mandatory, shouldn't be empty", HttpStatus.BAD_REQUEST);
 	}
-	
+
 	@ExceptionHandler(IllegalArgumentException.class)
-	public ResponseEntity<String> handlePriceField(){
-		return new ResponseEntity<String>("Price have to be a positive value greater than zero.", HttpStatus.BAD_REQUEST);
+	public ResponseEntity<String> handlePriceField(IllegalArgumentException ex) {
+		return new ResponseEntity<String>("Price have to be a positive value greater than zero.",
+				HttpStatus.BAD_REQUEST);
 	}
-	
+
 	@ExceptionHandler(NoSuchElementException.class)
-	public ResponseEntity<String> handleElementNotFound(){
-		return new ResponseEntity<String>("We are facing a shortage in this book.", HttpStatus.BAD_REQUEST);
+	public ResponseEntity<String> handleElementNotFoundExcpetin() {
+		return new ResponseEntity<String>("We are facing a shortage in this book.", HttpStatus.NOT_FOUND);
+	}
+
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		return new ResponseEntity<Object>("Please change your http reuest method to the appropriate one",
+				HttpStatus.METHOD_NOT_ALLOWED);
 	}
 }
